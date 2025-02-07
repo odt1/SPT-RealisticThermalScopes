8a637d0ea1fcd338b4dea64273f7ca1301dee044
odt1 <33424002+odt1@users.noreply.github.com>
2025-02-08 00:21:12 +0300

    scanline and sensitivity fixes
    - removed rolling scanline effect on FLIR, Echo1 and Zeus.
    - Fixed REAP-IR aim sensitivity and displayed zoom values.

--- src/mod.ts
+++ src/mod.ts
@@ -1,17 +1,30 @@
+/* eslint-disable @typescript-eslint/indent */
 import { DependencyContainer } from "tsyringe";
 
 import { IPostSptLoadMod } from "@spt/models/external/IPostSptLoadMod";
 import { ILogger } from "@spt/models/spt/utils/ILogger";
+import { DatabaseServer } from "@spt/servers/DatabaseServer";
+import { IDatabaseTables } from "@spt/models/spt/server/IDatabaseTables";
+import { ItemHelper } from "@spt/helpers/ItemHelper";
+import { BaseClasses } from "@spt/models/enums/BaseClasses";
 
-class Mod implements IPostSptLoadMod
-{
-    public postSptLoad(container: DependencyContainer): void
-    {
-        // get the logger from the server container
-        const logger = container.resolve<ILogger>("WinstonLogger");
+class Mod implements IPostSptLoadMod {
+	public postSptLoad(container: DependencyContainer): void {
+		// get database from server
+		const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
 
-        logger.info("Realistic Thermal Scopes loaded");
-    }
+		// Get all the in-memory json found in /assets/database
+		const tables: IDatabaseTables = databaseServer.getTables();
+		// get the logger from the server container
+		const logger = container.resolve<ILogger>("WinstonLogger");
+
+		const reapir = tables.templates.items["5a1eaa87fcdbcb001865f75e"];
+
+		reapir._props.Zooms[0] = [9.6, 1.2]; //zoom displayed in lower right corner. True zoom is x5.8 - x0.7
+		reapir._props.AimSensitivity[0] = [0.112, 0.93]; //sensitivity change. Calculated based on true zoom.
+
+		logger.info("Realistic Thermal Scopes loaded");
+	}
 }
 
 export const mod = new Mod();
